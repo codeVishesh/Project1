556f2d8d1c05524bd4d76fa69e8afe64f146b7fe
Vishesh Gupta <vishesh.gupta.jobs@gmail.com>
2025-12-10 01:31:30 +0530

    Add files via upload

---
+++ Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Services.css'
+
+export default function Services() {
+  return (
+    <section className="container services">
+      <h1>Services</h1>
+      <p>We offer web design, development, and digital strategy services to help your business grow.</p>
+      <ul>
+        <li>Custom Websites</li>
+        <li>UX / UI Design</li>
+        <li>Performance Optimization</li>
+        <li>Accessibility Audits</li>
+      </ul>
+    </section>
+  )
+}
